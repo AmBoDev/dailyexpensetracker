46257bf321308934e80aeabc0a4c795a684a8294
Aman <amanbohra885@gmail.com>
2025-09-23 21:18:31 +0530

    this is my project

---
+++ backend/expense/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class UserDetail(models.Model):
+    FullName = models.CharField(max_length=100)
+    Email = models.EmailField(max_length=100, unique=True)
+    Password = models.CharField(max_length=100)
+    RegDate = models.DateTimeField(auto_now_add=True)
+
+class Expense(models.Model):
+    UserId = models.ForeignKey(UserDetail, on_delete=models.CASCADE)
+    ExpenseDate = models.CharField(max_length=100)  # or models.DateField()
+    ExpenseItem = models.CharField(max_length=100)
+    ExpenseCost = models.CharField(max_length=100)
+    NoteDate = models.DateTimeField(auto_now_add=True)
